caa9f4e0bf6dd3b2d8aa7fb626b7230757f82143
Marishka95s <marishka95@ukr.net>
2021-01-15 20:40:46 +0200

    Header

--- js/menu-button.js
+++ js/menu-button.js
@@ -7,8 +7,8 @@
             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
         menuBtnRef.classList.toggle("is-open");
-            menuBtnRef.setAttribute("aria-expanded"), |expanded);
+        menuBtnRef.setAttribute("aria-expanded"), |expanded);
 
-            mobileMenuRef.classList.toggle("is-open");    
+        mobileMenuRef.classList.toggle("is-open");    
     });
 })();
